0babfea018e76f05c6e899c70cb016fd3bb29aa7
Duncan <duncan.nisbet@gmail.com>
2011-11-11 13:56:45 +0000

    HFJ worked examples

--- src/nisbet/duncan/animals/Dog.java
+++ src/nisbet/duncan/animals/Dog.java
@@ -1,23 +1,23 @@
 package nisbet.duncan.animals;
 
 
-public class Dog{
-    
+public class Dog extends Animal{
+
     private int size;
     String name;
-   
-    
+
+
     public int getSize() {
         return size;
     }
-    
+
     public void setSize(int s) {
         size = s;
     }
-    
+
     void bark () {
         if (size > 60){
-        System.out.println("Woof! Woof!");
+            System.out.println("Woof! Woof!");
         } else if (size > 14){
             System.out.println("Ruff! Ruff!");
         } else {
